b1dc3c3c6072966b6b1358293b907a79d36fb9a8
Rustam Agamaliev <agamalievrustam37@gmail.com>
2021-11-02 03:01:13 +0500

    Page transition change, Safari style fix

--- src/transitions/animate.ts
+++ src/transitions/animate.ts
@@ -6,7 +6,7 @@ const appGitName = settings['git-name'];
 export const animateTransition = (pathname: string, node: HTMLCollection) => {
     gsap.from(node, {
         opacity: 0,
-        xPercent: pathname === `/${appGitName}` ? -100 : 100,
+        xPercent: pathname === `/${appGitName}` ? 100 : -100,
         duration: 1,
         ease: "back.inOut(1)"
     });
